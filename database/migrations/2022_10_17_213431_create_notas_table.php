1d0df2ceda79c4ac20f46fdaba8214b90043383a
Ricardo Daniel Gasca <ricardodanielgasca@gmail.com>
2022-10-24 13:13:04 -0500

    se comentaron las lineas que contienen la foreign key esto es por lo mientras en lo qu ebusco la forma de enviar el formulario de notas con el valor de la foreing key ya establecido

--- database/migrations/2022_10_17_213431_create_notas_table.php
+++ database/migrations/2022_10_17_213431_create_notas_table.php
@@ -15,9 +15,10 @@ class CreateNotasTable extends Migration
     {
         Schema::create('notas', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('user_id')->references('id')->on('users');
             $table->string('title');
             $table->text('descripcion');
+            /*$table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');*/
             $table->timestamps();
         });
     }
